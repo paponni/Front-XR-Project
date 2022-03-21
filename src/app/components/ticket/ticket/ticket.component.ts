75995b513ed6dce390a35883f00e060bad520754
paponni <mohamedmamoun@hotmail.fr>
2022-03-21 01:05:37 +0100

    ticket component

--- src/app/components/ticket/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket/ticket.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketComponent implements OnInit {
 
+   TICKETS = [
+    {
+      nomMatch : "Real Madrid vs Valencia FC",
+      dateMatch : "Sunday 19 mai 2022",
+      heureMatch : "15h00",
+      lieuMatch : "Estadio el madrigol",
+      nbrTickets : "360",
+      photo : "../../../../assets/img/stade.png",
+      prix : 500
+    },
+    {
+      nomMatch : "Real Madrid vs Valencia FC",
+      dateMatch : "Sunday 19 mai 2022",
+      heureMatch : "15h00",
+      lieuMatch : "Estadio el madrigol",
+      nbrTickets : "360",
+      photo : "../../../../assets/img/stade.png",
+      prix : 500
+    },
+    {
+      nomMatch : "Real Madrid vs Valencia FC",
+      dateMatch : "Sunday 19 mai 2022",
+      heureMatch : "15h00",
+      lieuMatch : "Estadio el madrigol",
+      nbrTickets : "360",
+      photo : "../../../../assets/img/stade.png",
+      prix : 500
+    },
+    {
+      nomMatch : "Real Madrid vs Valencia FC",
+      dateMatch : "Sunday 19 mai 2022",
+      heureMatch : "15h00",
+      lieuMatch : "Estadio el madrigol",
+      nbrTickets : "360",
+      photo : "../../../../assets/img/stade.png",
+      prix : 500
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
