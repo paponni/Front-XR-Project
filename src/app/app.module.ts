ef15702741319104569efcbac23971df0c7a29eb
paponni <mohamedmamoun@hotmail.fr>
2022-03-26 18:17:14 +0100

    adding ticket component & enhancing style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { TicketComponent } from './components/ticket/ticket/ticket.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -52,6 +53,7 @@ import {MatIconModule} from '@angular/material/icon';
     MatCardModule,
     MatFormFieldModule,
     MatIconModule,
+    MatButtonModule,
     ToastrModule.forRoot()
 
   ],
